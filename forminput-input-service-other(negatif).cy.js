4b882202420ddd9af8725232f0446c24395d9941
@putri.sakinah <putri.sakinah@adidata.co.id>
2023-11-23 10:44:15 +0700

    first commit

---
+++ forminput-input-service-other(negatif).cy.js
@@ -0,0 +1,11 @@
+it('Assertions Demo', () => {
+
+    cy.visit('https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u')
+    cy.get(':nth-child(1) > .-cn-46 > .-a-50 > .-iP-61 > .-_F-60').type("Putri Sakinah")
+    cy.get(':nth-child(2) > .-cn-46 > .-a-50 > .-iP-61 > .-_F-60').type("082225943815")
+    cy.get('.-ni-67 > .-a-79 > .-hE-80').click()
+    cy.get('.-iP-85 > .-_F-60').type("testing")
+    cy.get('.-im-88').click()
+    cy.get('#DatePicker0-label').click().type('11/22/2023');
+    cy.get('.css-224').click();
+    })
---
+++ forminput-tanggalkosong(negative).cy.js
@@ -0,0 +1,12 @@
+it('Assertions Demo', () => {
+
+    cy.visit('https://forms.office.com/pages/responsepage.aspx?id=is2XW8LLaEmfFhLKD9VwE9lpKmxdveNGmMWKETZvAWNUMzhBV1lYTlc1SDNRS00xRVg4OFhPODlQTS4u')
+    cy.get(':nth-child(1) > .-cn-46 > .-a-50 > .-iP-61 > .-_F-60').type("Putri Sakinah")
+    cy.get(':nth-child(2) > .-cn-46 > .-a-50 > .-iP-61 > .-_F-60').type("082225943815")
+    cy.get(':nth-child(1) > div > .--x-77 > .-a-79 > .-hE-80').click().check('Affordable')
+    cy.get('.-im-88').click();
+    // cy.get('#DatePicker0-label').click().type('11/22/2023');
+    cy.get('.css-224').click();
+    cy.get('.-a-229')
+    cy.get('#ErrorMsgId_r1abee94394494318b1e34a419838e56c > span')
+})
